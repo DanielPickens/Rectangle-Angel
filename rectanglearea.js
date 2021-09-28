2e4c935cfcfda42bb55d5a07e965c42db4285dd2
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-09-28 16:27:45 -0600

    Update rectanglearea.js

--- rectanglearea.js
+++ rectanglearea.js
@@ -5,3 +5,12 @@ var computeArea = function(A, B, C, D, E, F, G, H) {
         const downMax = Math.max(B, F);
         return (C - A) * (D - B) - (rightMin < leftMax ? 0 : rightMin - leftMax) * (upMin < downMax ? 0 : upMin - downMax) + (G - E) * (H - F);
     }
+
+
+
+/*
+Success
+Details 
+Runtime: 124 ms, faster than 90.91% of JavaScript online submissions for Rectangle Area.
+Memory Usage: 44.7 MB, less than 46.75% of JavaScript online submissions for Rectangle Area.
+*/
